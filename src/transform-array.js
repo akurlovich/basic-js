e018c50e688d50e49fb1a0a91ec2936035c80003
akurlovich <71250705+akurlovich@users.noreply.github.com>
2021-03-02 22:07:59 +0300

    feat: add double function to transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,7 +1,6 @@
-// const CustomError = require("../extensions/custom-error");
+const CustomError = require("../extensions/custom-error");
 
-// module.exports = 
-function transform(array) {
+module.exports = function transform(array) {
   if (!(array instanceof Array)) {
     throw new CustomError('Not implemented')};
   let dn = '--discard-next',
@@ -10,26 +9,48 @@ function transform(array) {
       ddp = '--double-prev',
       newArr = [];
   for (let i = 0; i < array.length; i++) {
-    if ((+array[i])) {
-      newArr.push(array[i])
-    } else if (array[i] == dn) {
-      newArr.push((array[i + 1] * 2));
+    // if ((+array[i])) {
+    //   newArr.push(array[i])
+    // } else if (array[i] == dn) {
+    //   // newArr.push(array[i + 1]);
+    //   i++;
+    // } else if (array[i] == ddn) {
+    //     newArr.push((array[i + 1] ** 2));
+    //     i++;
+    // } else if (array[i] == dp) {
+    //   newArr.pop();
+    // } else if (array[i] == ddp) {
+    //   // console.log(newArr[i - 1]);
+    //   // console.log(newArr[i - 1]);
+    //   newArr.pop();
+    //   newArr.push((array[i - 1] ** 2));
+    // }
+    if (array[i] == dn) {
+      // newArr.push(array[i + 1]);
       i++;
-    } else if (array[i] == ddn) {
-        newArr.push((array[i + 1] ** 2));
-        i++;
-    } else if (array[i] == dp) {
-      newArr[i - 1] = ((array[i - 1] ** 2));
+    } else
+    if (array[i] == ddn) {
+      newArr.push((array[i + 1] ** 2));
       i++;
-    } else if (array[i] == ddp) {
-      newArr[i - 1] = ((array[i - 1] ** 2))
-      i++;
-    }
+    } else
+    if (array[i] == dp) {
+      newArr.pop();
+    } else
+    if (array[i] == ddp) {
+      // console.log(newArr[i - 1]);
+      // console.log(newArr[i - 1]);
+      newArr.pop();
+      newArr.push((array[i - 1] ** 2));
+    } else
+    newArr.push(array[i])
   }
-  // return newArr;
-  console.log(newArr);
+  return newArr;
+  // console.log(newArr);
+  // for (let i = 0; i < newArr.length; i++) {
+  //   console.log(typeof newArr[i])
+  // }
   
   // remove line with error and write your code here
 };
-let array = [1, 3, 5, '--discard-next', 6, 11, 4, '--discard-prev', 3, 3, 7, 18, '--double-next', 5, 8, '--double-prev'];
-transform(array);
+// let array = [1, 3, 5, '--discard-next', 6, 11, 4, '--discard-prev', 3, 3, 7, 18, '--double-next', 5, 8, '--double-prev'];
+// transform(array);
