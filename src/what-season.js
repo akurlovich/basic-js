5fa1aa157df3e30e3a28cec8d0621754ddda1e37
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-03-02 16:47:11 +0300

    fix: date in season

--- src/what-season.js
+++ src/what-season.js
@@ -1,20 +1,24 @@
-// const CustomError = require("../extensions/custom-error");
+const CustomError = require("../extensions/custom-error");
 
-// module.exports = 
-function getSeason(date) {
+module.exports = function getSeason(date) {
+  let season = '';
   if (typeof date == 'undefined') {
     let message = 'Unable to determine the time of year!';
-    console.log(message);
+    return message;
   } else if (!(date instanceof Date)) {
         return false;
       } else {
       month = date.getMonth();
-      console.log(month);
+      // console.log(month);
+      if (month == 1 || month == 2 || month == 0) {season = 'winter'}
+        else if (month >= 3 && month <= 5) {season = 'spring'}
+          else if (month >= 6 && month <= 8) {season = 'summer'}
+            else if (month >= 9 && month <= 11) {season = 'autumn'};
+              // else season = 'error';
       // throw new CustomError('Not implemented');
       // remove line with error and write your code here
     };
+    return season;
 }
-
-
-const date = new Date(2020, 10, 31)
-getSeason(date);
+// const date = new Date(2020, 15, 31)
+// getSeason(date);
