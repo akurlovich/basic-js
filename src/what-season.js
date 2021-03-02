846db5e40bb3de94bfa4fe92b817cc28e40396d6
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-03-02 15:24:22 +0300

    feat: add season function

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,20 @@
-const CustomError = require("../extensions/custom-error");
+// const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+// module.exports = 
+function getSeason(date) {
+  if (typeof date == 'undefined') {
+    let message = 'Unable to determine the time of year!';
+    console.log(message);
+  } else if (!(date instanceof Date)) {
+        return false;
+      } else {
+      month = date.getMonth();
+      console.log(month);
+      // throw new CustomError('Not implemented');
+      // remove line with error and write your code here
+    };
+}
+
+
+const date = new Date(2020, 10, 31)
+getSeason(date);
