d6f92367df024c617f065f788c862c558f54669f
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-03-02 09:50:46 +0300

    feat: complite count_cat function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,13 +1,21 @@
 // const CustomError = require("../extensions/custom-error");
 
-// module.exports = 
-function countCats(matrix) {
+module.exports = function countCats(matrix) {
   let count = 0;
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
-
+      if (matrix[i][j] == '^^') {
+        count++;
+        // console.log(count);
+      } 
     }
   }
+  // console.log(count);
+  return count;
   // throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
+
+// let matrix = [['##', 'dd', '00'], ['^^', '..', 'ss'], ['AA', '^^', 'Oo']];
+// countCats(matrix);
+
