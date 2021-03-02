4c3b9f23488d0f3d5da0ec415a28ec1853b2671b
akurlovich <71250705+akurlovich@users.noreply.github.com>
2021-03-02 20:16:32 +0300

    feat: add transform array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,9 @@
-const CustomError = require("../extensions/custom-error");
+// const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
+// module.exports = 
+function transform(arr) {
+  if (!(array instanceof Array)) {
+    throw new CustomError('Not implemented')};
+  
   // remove line with error and write your code here
 };
