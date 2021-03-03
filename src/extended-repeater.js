99622482052965b3fc6cebf6a921f3e220ad0d2a
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-03-03 14:53:54 +0300

    fix: repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,9 +1,32 @@
-// const CustomError = require("../extensions/custom-error");
+const CustomError = require("../extensions/custom-error");
 
-// module.exports = 
-function repeater(/* str, options */) {
-
+module.exports = function repeater(str, options) {
+  let repeatTime = options.repeatTimes,
+      separator = options.separator,
+      addition = options.addition,
+      additionRepeatTimes = options.additionRepeatTimes,
+      additionSeparator = options.additionSeparator;
+  // str.toString();
+  let newStr = [];
+  if (typeof separator == 'undefined') {separator = '+'}
+  if (typeof addition == 'undefined') {addition = ''}
+  if (typeof repeatTime == 'undefined') {repeatTime = 1}
+  while(newStr.length < (repeatTime * 3)) {
+    newStr.push(str);
+    // newStr.push(addition + separator);
+    newStr.push(addition);
+    newStr.push(separator);
+  }
+  // if (addition == '') {
+    newStr.pop();
+  
+  
+  return newStr.join('');
+  // return newStr;
+  // console.log(newStr.join(''), repeatTime);
+  // console.log(separator);
   // throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
-  
+
+// repeater('hello', {repeatTimes: 3, separator: 's'})
