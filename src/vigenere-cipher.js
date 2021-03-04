b7cb40bbb9b80057031f291d01759d52c4ed2665
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-03-04 13:26:43 +0300

    fix: vegener function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -24,21 +24,55 @@ class VigenereCipheringMachine {
         keyLength = keyUCDried.length,
         msgLength = messageUCDried.length;
 
-        for (let i = 0, n = 0; i < msgLength; i++, n++) {
-          let messageChar = messageUCDried[i];
-          if (!alphabet.indexOf(messageChar)) {
-            res = push(messageChar);
-            n--;
-            continue;
-          }
+    for (let i = 0, n = 0; i < msgLength; i++, n++) {
+      let messageChar = messageUCDried[i];
+      if (!alphabet.indexOf(messageChar)) {
+        res = push(messageChar);
+        n--;
+        continue;
+      }
 
-          let messageCharCode = messageChar.charCodeAt(0),
-              shiftedRowIndex = keyUCDried.charCodeAt(n % keyLength) - START_POSITION_CHARCODE,
-              shiftedCharIndex = messageCharCode - START_POSITION_CHARCODE;
+      let messageCharCode = messageChar.charCodeAt(0),
+          shiftedRowIndex = keyUCDried.charCodeAt(n % keyLength) - START_POSITION_CHARCODE,
+          shiftedCharIndex = messageCharCode - START_POSITION_CHARCODE;
 
-          res.push(tabulaRecta[shiftedRowIndex][shiftedCharIndex]);
+      res.push(tabulaRecta[shiftedRowIndex][shiftedCharIndex]);
+    }
+    if (this._reverseFlag === true) return res.reverse().join('');
+    return res.join('');
+  }
+
+  decrypt(ecryptedMessage, key) {
+    if (ecryptedMessage === undefined || key === undefined) throw new CustomError('Not implemented');
+    let keyUC = key.toUpperCase(),
+        keyUCDried = keyUC.replace(/\s/g, ''),
+        ecryptedMessageUC = ecryptedMessage.toUpperCase(),
+        ecryptedMessageUCDreid = ecryptedMessageUC.trim(),
+        keyLength = keyUCDried.length,
+        msgLength = ecryptedMessageUCDreid.length,
+        res = [];
+    
+      for (let i = 0, n = 0; i < msgLength; i++, n++) {
+        let ecryptedMessageChar = ecryptedMessageUCDreid[i],
+            rowIndex = keyUCDried.charCodeAt(n % keyLength) - START_POSITION_CHARCODE,
+            row = tabulaRecta[rowIndex];
+        if (!alphabet.indexOf(ecryptedMessageChar)) {
+          res.push(ecryptedMessageChar);
+          n--;
+          continue;
         }
 
+        let targetIndex = row.indexOf(ecryptedMessageChar),
+            unshiftedRow = tabulaRecta[0],
+            trueChar = unshiftedRow[targetIndex];
+
+        res.push(trueChar);
+      }
+
+      if (this._reverseFlag === true) return res.reverse().join('');
+      return res.join('');
+
+    
   }
 }
 
