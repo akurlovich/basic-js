aa38a58ffb0512fd2881091435f80c35b81da2d5
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-03-04 12:56:39 +0300

    feat: add vieneger coder function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -13,14 +13,33 @@ class VigenereCipheringMachine {
     this._reverseFlag = !isDirect;
   }
   
-  // encrypt() {
-  //   throw new CustomError('Not implemented');
-  //   // remove line with error and write your code here
-  // }    
-  // decrypt() {
-  //   throw new CustomError('Not implemented');
-  //   // remove line with error and write your code here
-  // }
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) throw new CustomError('Not implemented');
+    
+    let keyUC = key.toUpperCase(),
+        keyUCDried = keyUC.replace(/\s/g, ''),
+        messegeUC = message.toUpperCase(),
+        messageUCDried = messegeUC.trim(),
+        res = [],
+        keyLength = keyUCDried.length,
+        msgLength = messageUCDried.length;
+
+        for (let i = 0, n = 0; i < msgLength; i++, n++) {
+          let messageChar = messageUCDried[i];
+          if (!alphabet.indexOf(messageChar)) {
+            res = push(messageChar);
+            n--;
+            continue;
+          }
+
+          let messageCharCode = messageChar.charCodeAt(0),
+              shiftedRowIndex = keyUCDried.charCodeAt(n % keyLength) - START_POSITION_CHARCODE,
+              shiftedCharIndex = messageCharCode - START_POSITION_CHARCODE;
+
+          res.push(tabulaRecta[shiftedRowIndex][shiftedCharIndex]);
+        }
+
+  }
 }
 
 module.exports = VigenereCipheringMachine;
