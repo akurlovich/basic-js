aa15871f19a538d4641c9cda38d80943660d5e71
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-03-04 16:25:52 +0300

    fix: dream team function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,31 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(array) {
-  
-  if (!(array instanceof Array)) {
-    return false;
-  } else 
-  if (array.length == 0) {
-    return false;
-  } else {
-  
-  let newArr = [],
-      finArr = [];
-  for (let i = 0; i < array.length; i++) {
-    if (typeof array[i] == 'string') {
-      newArr = array[i].trim().toUpperCase().split('');
-      console.log(newArr);
-      console.log(newArr[0]);
-      finArr.push(newArr[0]);
-    }
+  if (!(array instanceof Array)) return false;
 
+  let finArr = [];
+      len = array.length;
+  for (let i = 0; i < len; i ++) {
+    if (typeof array[i] === 'string') {
+      let member = array[i].trim();
+      finArr.push(member[0].toUpperCase());
+    }
   }
+  // if (!(array instanceof Array)) {
+  //   return false;
+  // } else 
+  // if (array.length == 0) {
+  //   return false;
+  // } else {
+  
+  // let newArr = [],
+  //     finArr = [];
+  // for (let i = 0; i < array.length; i++) {
+  //   if (typeof array[i] == 'string') {
+  //     newArr = array[i].trim().toUpperCase().split('');
+  //     console.log(newArr);
+  //     console.log(newArr[0]);
+  //     finArr.push(newArr[0]);
+  //   }
+
+  // }
   // throw new CustomError('Not implemented');
   // remove line with error and write your code here
   // finArr.join('');
   // console.log(finArr.sort().join(''));
   return (finArr.sort().join(''));
-  }
 };
 
 // let array = ['amelia', 'Yrace', 111, 'Lily', 'Ruby'];
--- src/what-season.js
+++ src/what-season.js
@@ -1,28 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(date) {
-  let season = '';
-  if (typeof date == 'undefined') {
-    let message = 'Unable to determine the time of year!';
-    return message;
-  } 
-  // else if (isNaN(Date.parse(date)))
-  else if (!(date instanceof Date)) 
-  {
-    throw new CustomError('Not implemented');
-        // return false;
-      } else {
-      month = date.getMonth();
-      // console.log(month);
-      if (month >= 11 || month <= 1) {season = 'winter'}
-        else if (month <= 4) {season = 'spring'}
-          else if (month <= 7) {season = 'summer'}
-            else season = 'autumn';
-              // else season = 'error';
-      // throw new CustomError('Not implemented');
-      // remove line with error and write your code here
-    };
+module.exports = function getSeason(date = 'no-value') {
+  if (date === 'no-value') return 'Unable to determine the time of year!';
+  if (date instanceof Date) {
+    // if (Object.prototype.toString.call(data) !== '[object Data]') throw new Error('Caught fake data!');
+    
+    let season = '',
+        month = date.getMonth();
+    // console.log(month);
+    if (month >= 11 || month <= 1) {season = 'winter'}
+      else if (month <= 4) {season = 'spring'}
+        else if (month <= 7) {season = 'summer'}
+          else season = 'autumn';
     return season;
-}
+
+    // let season = '';
+    // if (typeof date == 'undefined') {
+    //   let message = 'Unable to determine the time of year!';
+    //   return message;
+    // } 
+    // // else if (isNaN(Date.parse(date)))
+    // else if (!(date instanceof Date)) 
+    // {
+    //   throw new CustomError('Not implemented');
+    //       // return false;
+    //     } else {
+    //     month = date.getMonth();
+    //     // console.log(month);
+    //     if (month >= 11 || month <= 1) {season = 'winter'}
+    //       else if (month <= 4) {season = 'spring'}
+    //         else if (month <= 7) {season = 'summer'}
+    //           else season = 'autumn';
+    //             // else season = 'error';
+    //     // throw new CustomError('Not implemented');
+    //     // remove line with error and write your code here
+    //   };
+    //   return season;
+  }
+  throw new Error('Wrong argument passed!');
+};
 // const date = new Date(2020, 15, 31)
 // getSeason(date);
