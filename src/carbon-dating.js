ab5b6fd83e0d3cb4126ba46e5ee669ac85302461
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-03-04 07:53:02 +0300

    feat: add carbon function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,18 @@
+const { match } = require("sinon");
 const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+module.exports = function dateSample(sampleActivity) {
+  if (sampleActivity === 'no-value') return false;
+  if (typeof sampleActivity !== 'string') return false;
+  let numeric = parseFloat(sampleActivity);
+  if (isNaN(numeric)) return false;
+  if (numeric > MODERN_ACTIVITY) return false;
+  if (numeric === 0 || numeric < 0) return false;
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / numeric) / k);
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
