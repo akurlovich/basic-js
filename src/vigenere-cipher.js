cda77637b0feb5fb67266e89e502aad1a7bc2a1f
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-03-04 11:20:52 +0300

    feat: add vigerer function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,26 @@
 const CustomError = require("../extensions/custom-error");
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+const tabulaRecta = [
+
+];
+
+const START_POSITION_CHARCODE = 65;
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(isDirect = true) {
+    this._reverseFlag = !isDirect;
   }
+  
+  // encrypt() {
+  //   throw new CustomError('Not implemented');
+  //   // remove line with error and write your code here
+  // }    
+  // decrypt() {
+  //   throw new CustomError('Not implemented');
+  //   // remove line with error and write your code here
+  // }
 }
 
 module.exports = VigenereCipheringMachine;
