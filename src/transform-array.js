a0766aedf170795a4b77f5353203686f40962479
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-03-05 16:08:06 +0300

    feat: transforn array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -9,48 +9,72 @@ module.exports = function transform(array) {
       ddp = '--double-prev',
       newArr = [];
   for (let i = 0; i < array.length; i++) {
-    // if ((+array[i])) {
-    //   newArr.push(array[i])
-    // } else if (array[i] == dn) {
-    //   // newArr.push(array[i + 1]);
-    //   i++;
-    // } else if (array[i] == ddn) {
-    //     newArr.push((array[i + 1] ** 2));
-    //     i++;
-    // } else if (array[i] == dp) {
-    //   newArr.pop();
-    // } else if (array[i] == ddp) {
-    //   // console.log(newArr[i - 1]);
-    //   // console.log(newArr[i - 1]);
-    //   newArr.pop();
-    //   newArr.push((array[i - 1] ** 2));
-    // }
-    if (array[i] == dn) {
-      // newArr.push(array[i + 1]);
-      i++;
-    } else
+    
+    if (array[i] == dn) {i++}
+      // else newArr.push(array[i]);
+    else 
     if (array[i] == ddn) {
-      newArr.push((array[i + 1] ** 2));
-      i++;
-    } else
-    if (array[i] == dp) {
-      newArr.pop();
-    } else
-    if (array[i] == ddp) {
-      // console.log(newArr[i - 1]);
-      // console.log(newArr[i - 1]);
-      newArr.pop();
-      newArr.push((array[i - 1] ** 2));
-    } else
-    newArr.push(array[i])
+      newArr.push(array[i + 1]);
+      newArr.push(array[i + 1]);
+      i++;}
+      // else newArr.push(array[i]);
+    else 
+    if (array[i] == dp) {newArr.pop();}
+      // else newArr.push(array[i]);
+    else 
+    if ((array[i] == ddp) && (array[i - 2] !== dn)) {
+        newArr.pop();
+        newArr.push(array[i - 1]);}
+      else newArr.push(array[i]);
+
+
+
+
+  //   if (array[i] == dn) {
+  //     // newArr.push(array[i + 1]);
+  //     // console.log(array[i], i);
+  //     i++;
+  //   } else
+  //   if (array[i] == ddn) {
+  //     newArr.push((array[i + 1] ** 2));
+  //     console.log('ddn');
+  //     i++;
+  //   } else
+  //   if (array[i] == dp) {
+  //     // console.log(newArr + 'dp');
+  //     newArr.pop();
+  //   } else
+  //   if ((array[i] == ddp) && (array[i - 2] !== dn)) {
+  //     // console.log(newArr[i - 1]);
+  //     // console.log(newArr[i - 1]);
+  //       // newArr.pop();
+  //     newArr.push(array[i - 1]);
+  //     console.log(newArr);
+
+
+  //   } else
+  //   newArr.push(array[i])
+  // }
+
+
+  // for (let n = 0; n < newArr.length; n++) {
+  //   // if (newArr[n] === NaN) {
+  //   if (Number.isNaN(newArr[n])) {
+  //     newArr.splice(n, 1);
+  //   }
+  // }
+
+  // 
+  
+  
+  };
+
+  for (let n = 0; n < newArr.length; n++) {
+      if (newArr[n] == ddp) {newArr.splice(n, 1)}
   }
   return newArr;
   // console.log(newArr);
-  // for (let i = 0; i < newArr.length; i++) {
-  //   console.log(typeof newArr[i])
-  // }
-  
-  // remove line with error and write your code here
 };
-// let array = [1, 3, 5, '--discard-next', 6, 11, 4, '--discard-prev', 3, 3, 7, 18, '--double-next', 5, 8, '--double-prev'];
+
+// let array = [1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5, 8, 11, '--double-next', 1337, '--double-prev', 4, 5];
 // transform(array);
